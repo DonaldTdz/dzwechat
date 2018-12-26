3068e91c12a33628afa8e78d6d8ca3aa9be58347
DonaldTdz <tangdezou@qq.com>
2018-12-26 11:55:33 +0800

    集成MySQL

--- aspnet-core/src/hc.dzwechat.EntityFrameworkCore/EntityFrameworkCore/dzwechatDbContextConfigurer.cs
+++ aspnet-core/src/hc.dzwechat.EntityFrameworkCore/EntityFrameworkCore/dzwechatDbContextConfigurer.cs
@@ -7,12 +7,14 @@ namespace hc.dzwechat.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<dzwechatDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            //builder.UseSqlServer(connectionString);
+            builder.UseMySql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<dzwechatDbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            //builder.UseSqlServer(connection);
+            builder.UseMySql(connection);
         }
     }
 }
