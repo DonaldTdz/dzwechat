a6f8276f7094578ecb8231caacb04191d764b689
shiguanWXX <1358110409@qq.com>
2018-12-29 10:03:29 +0800

    修改冲突

--- HC.DZWechat/aspnet-core/src/hc.dzwechat.EntityFrameworkCore/EntityFrameworkCore/DZWechatDbContext.cs
+++ HC.DZWechat/aspnet-core/src/hc.dzwechat.EntityFrameworkCore/EntityFrameworkCore/DZWechatDbContext.cs
@@ -3,12 +3,9 @@ using Abp.Zero.EntityFrameworkCore;
 using HC.DZWechat.Authorization.Roles;
 using HC.DZWechat.Authorization.Users;
 using HC.DZWechat.MultiTenancy;
-<<<<<<< HEAD
 using HC.DZWechat.Newses;
-=======
 using HC.DZWechat.WechatMessages;
 using HC.DZWechat.WechatSubscribes;
->>>>>>> f5786a63f93dd2c298064b2917b8e6db1b1801b9
 
 namespace HC.DZWechat.EntityFrameworkCore
 {
