91052aa47eac28f9cc1b764fd9905310c5e6b803
YangFan17 <173838362@qq.com>
2019-01-25 16:20:36 +0800

    模板消息

--- HC.DZWechat/aspnet-core/src/hc.dzwechat.Core/ScanExchange/ScanExchangeManager.cs
+++ HC.DZWechat/aspnet-core/src/hc.dzwechat.Core/ScanExchange/ScanExchangeManager.cs
@@ -10,6 +10,11 @@ using HC.DZWechat.OrderDetails;
 using HC.DZWechat.ScanExchange.Dtos;
 using HC.DZWechat.CommonDto;
 using HC.DZWechat.Exchanges;
+using Senparc.Weixin.MP.AdvancedAPIs.TemplateMessage;
+using Senparc.Weixin.WxOpen.AdvancedAPIs.Template;
+using Senparc.Weixin;
+using HC.DZWechat.Shops;
+using HC.DZWechat.WechatConfigs;
 
 namespace HC.DZWechat.ScanExchange
 {
@@ -19,17 +24,23 @@ namespace HC.DZWechat.ScanExchange
         private readonly IRepository<WechatUser, Guid> _wechatuserRepository;
         private readonly IRepository<OrderDetail, Guid> _orderDetailRepository;
         private readonly IRepository<Exchange, Guid> _exchangeRepository;
+        private readonly IRepository<Shop, Guid> _shopRepository;
+        private readonly IRepository<WechatConfig, Guid> _configRepository;
 
         public ScanExchangeManager(IRepository<Order, Guid> repository
             , IRepository<OrderDetail, Guid> orderDetailRepository
             , IRepository<WechatUser, Guid> wechatuserRepository
             , IRepository<Exchange, Guid> exchangeRepository
+            , IRepository<Shop, Guid> shopRepository
+            , IRepository<WechatConfig, Guid> configRepository
         )
         {
             _repository = repository;
             _orderDetailRepository = orderDetailRepository;
             _wechatuserRepository = wechatuserRepository;
             _exchangeRepository = exchangeRepository;
+            _shopRepository = shopRepository;
+            _configRepository = configRepository;
         }
 
 
@@ -40,7 +51,7 @@ namespace HC.DZWechat.ScanExchange
         /// <returns></returns>
         public bool GetIsAttentionByIdAsync(string openId)
         {
-            int weChat =  _wechatuserRepository.GetAll().Where(v => v.OpenId == openId && v.BindStatus == DZEnums.DZCommonEnums.BindStatus.取消关注).Count();
+            int weChat = _wechatuserRepository.GetAll().Where(v => v.OpenId == openId && v.BindStatus == DZEnums.DZCommonEnums.BindStatus.取消关注).Count();
             if (weChat == 0)
             {
                 return true;
@@ -89,9 +100,9 @@ namespace HC.DZWechat.ScanExchange
         /// </summary>
         /// <param name="orderId"></param>
         /// <returns></returns>
-        public async Task<APIResultDto> ExchangeGoods(Guid orderId,string openId,Guid shopId)
+        public async Task<APIResultDto> ExchangeGoods(Guid orderId, string openId, Guid shopId)
         {
-            Guid userId =await _wechatuserRepository.GetAll().Where(v => v.OpenId == openId).Select(v => v.Id).FirstOrDefaultAsync();
+            Guid userId = await _wechatuserRepository.GetAll().Where(v => v.OpenId == openId).Select(v => v.Id).FirstOrDefaultAsync();
             var orderDetails = await _orderDetailRepository.GetAll().Where(v => v.OrderId == orderId && v.Status == DZEnums.DZCommonEnums.ExchangeStatus.未兑换).ToListAsync();
             foreach (var item in orderDetails)
             {
@@ -104,6 +115,11 @@ namespace HC.DZWechat.ScanExchange
             {
                 await UpdateOrderStatus(orderId);
             }
+            //发送模板消息
+            var orderInfo = await _repository.GetAsync(orderId);
+            string wxOpenId = await _wechatuserRepository.GetAll().Where(v => v.Id == orderInfo.UserId).Select(v => v.WxOpenId).FirstOrDefaultAsync();
+            string shopName = await _shopRepository.GetAll().Where(v => v.Id == shopId).Select(v => v.Name).FirstOrDefaultAsync();
+            await OrderInfoMesssage(wxOpenId, orderInfo.Number, shopName);
             return new APIResultDto() { Code = 0, Msg = "兑换成功" };
         }
 
@@ -163,7 +179,7 @@ namespace HC.DZWechat.ScanExchange
         /// <param name="shopId"></param>
         /// <param name="openId"></param>
         /// <returns></returns>
-        protected virtual async Task UpdateExchangeAsync(Guid orderDetailId,Guid shopId,Guid userId)
+        protected virtual async Task UpdateExchangeAsync(Guid orderDetailId, Guid shopId, Guid userId)
         {
             var entity = new Exchange();
             entity.CreationTime = DateTime.Now;
@@ -172,6 +188,39 @@ namespace HC.DZWechat.ScanExchange
             entity.WechatUserId = userId;
             entity.OrderDetailId = orderDetailId;
             await _exchangeRepository.InsertAsync(entity);
+        }
+
+        /// <summary>
+        /// 兑换成功通知
+        /// </summary>
+        /// <param name="wxOpenId"></param>
+        /// <param name="orderNo"></param>
+        /// <param name="shopName"></param>
+        /// <returns></returns>
+        private async Task OrderInfoMesssage(string wxOpenId, string orderNo, string shopName)
+        {
+            try
+            {
+                string appId = Config.SenparcWeixinSetting.WxOpenAppId;//与微信公众账号后台的AppId设置保持一致，区分大小写。
+                string templateId = await _configRepository.GetAll().Select(v => v.TemplateIds).FirstOrDefaultAsync();
+                //string templateId = "hO16T1Sh_0R_7ttTqkCbpECOXfPEIOk3f9T6Of_ohYQ";
+                if (!string.IsNullOrEmpty(templateId))
+                {
+                    string[] ids = templateId.Split(',');
+                    object data = new
+                    {
+                        keyword1 = new TemplateDataItem(orderNo),//订单编号
+                        keyword2 = new TemplateDataItem(DateTime.Now.ToString("yyyy-MM-dd HH:mm")),//兑换时间
+                        keyword3 = new TemplateDataItem(shopName),//商家名称
+                        keyword4 = new TemplateDataItem("恭喜您兑换成功，请核实商品信息")//备注信息
+                    };
+                    await TemplateApi.SendTemplateMessageAsync(appId, wxOpenId, ids[1], data, "formSubmit");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.ErrorFormat("兑换成功发送消息通知失败 error：{0} Exception：{1}", ex.Message, ex);
+            }
         }
     }
 }
